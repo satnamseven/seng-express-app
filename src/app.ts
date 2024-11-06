5b73254dfdd948b917349e28ba796476ef9842bc
= <=>
2024-11-05 22:50:34 -0500

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,22 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get('/records/:id?', async (req, res): Promise<void> => {
+
+  const { id } = req.params;
+
+  res.status(200).json({ message: 'Id was sent as params' });
+  return;
+
 });
+
+app.post('/records', async (req, res): Promise<void> => {
+
+  const { name, age } = req.body;
+  //add send msg
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
